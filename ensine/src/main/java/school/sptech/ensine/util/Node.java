caf2113de019bc9d060d55cfce29aab601c2e9cb
GabrielVannucchi <gabriel.correa@sptech.school>
2023-08-31 21:30:47 -0300

    hash quebrada

---
+++ ensine/src/main/java/school/sptech/ensine/util/Node.java
@@ -0,0 +1,35 @@
+package school.sptech.ensine.util;
+
+public class Node {
+    private int info;
+    private Node next;
+
+    public Node(int info) {
+        this.info = info;
+        this.next = null;
+    }
+
+    public int getInfo() {
+        return info;
+    }
+
+    public void setInfo(int info) {
+        this.info = info;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "info=" + info +
+                ", next=" + next +
+                '}';
+    }
+}
