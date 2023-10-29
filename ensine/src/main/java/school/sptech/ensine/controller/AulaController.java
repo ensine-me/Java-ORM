fa1ab14504bbbe84799d1c2977aa541c5aaf2139
HugoHanashiro <hugo.galdino@sptech.school>
2023-10-29 20:12:21 -0300

    fix: cors

--- ensine/src/main/java/school/sptech/ensine/controller/AulaController.java
+++ ensine/src/main/java/school/sptech/ensine/controller/AulaController.java
@@ -238,7 +238,6 @@ public class AulaController {
         }
         return ResponseEntity.status(404).body("Aula não encontrada");
     }
-    @CrossOrigin("http://localhost:3000")
     @PatchMapping("/{id}/mudanca-status")
     public ResponseEntity<Aula> mudarStatus(@PathVariable int id, @RequestParam Status status) {
         return ResponseEntity.of(aulaService.atualizarStatusAula(id, status));
